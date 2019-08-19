506f615a733ccdc7ddaa77b3ab78d981336fadca
raaamen <sarahapplebaum.sia@gmail.com>
2019-08-19 12:14:23 -0400

    gameManager
    init score

--- gameJamProject_SKTLH/Assets/Scripts/GameManager.cs
+++ gameJamProject_SKTLH/Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+    public int playerScore;
 
 	// Use this for initialization
 	void Start () {
