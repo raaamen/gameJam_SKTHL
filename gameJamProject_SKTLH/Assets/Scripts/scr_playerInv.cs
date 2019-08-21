a39a07e3443b4b642f909540e577911a5d48c689
raaamen <sarahapplebaum.sia@gmail.com>
2019-08-21 16:17:21 -0400

    End of day commit

---
+++ gameJamProject_SKTLH/Assets/Scripts/scr_playerInv.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_playerInv : MonoBehaviour {
+
+    public List<Trash> inventoryFull;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void addRecycle()
+    {
+        Recycle recycling = new Recycle();
+        if (inventoryFull.Count<1)
+        {
+            inventoryFull.Add(recycling);
+        }
+        else
+        {
+            player.GetComponent<scr_playerMovement>().invFull = true;
+        }
+    }
+    public void depositAllTrash()
+    {
+        foreach (var item in inventoryFull)
+        {
+            GetComponent<GameManager>().totalTrash--;
+        }
+        inventoryFull.Clear();
+    }
+
+
+}
---
+++ gameJamProject_SKTLH/Assets/Scripts/startButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startButton : MonoBehaviour {
+
+    public GameObject manager;
+    public GameObject titleScreen;
+
+	// Use this for initialization
+    public void start()
+    {
+        titleScreen.SetActive(false);
+        manager.GetComponent<scr_camManage>().currentArea = "Home";
+    }
+
+}
